0096c3c850d443ea1ca00863cfa0217588e326f9
bs180 <97455015+bs180@users.noreply.github.com>
2026-06-07 21:55:00 +0200

    Delete src directory

--- src/gameplay/CollisionSystem.js
+++
@@ -1,8 +0,0 @@
-export function isColliding(a, b){
-  const dx = a.position.x - b.position.x;
-  const dy = a.position.y - b.position.y;
-
-  const radiusSum = a.radius + b.radius;
-
-  return dx * dx + dy * dy <= radiusSum * radiusSum;
-}
